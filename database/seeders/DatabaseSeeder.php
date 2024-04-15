bd2247daf389952fb1483ec346caf66d7ae17af2
Adria Jofre Cantón <ajofre@cendrasso.net>
2024-04-15 19:57:52 +0200

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        
+        Course::factory(10)->create();
 
         Content::factory(200)->create();
 
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -40,6 +40,11 @@ const showingNavigationDropdown = ref(false);
                                     Gestió de cursos
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('contents.index')" :active="route().current('contents.index')">
+                                    Gestió de contingut
+                                </NavLink>
+                            </div>
 
                         </div>
 
